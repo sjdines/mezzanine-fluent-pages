35c264819bac12fcb3baf8a2a33d63dd916f5f86
Stuart Dines <me@stuartdines.com>
2016-10-24 21:06:45 +1100

    Remove keyword argument and allow generic argument passing.

--- mezzanine_fluent_pages/mezzanine_layout_page/widgets.py
+++ mezzanine_fluent_pages/mezzanine_layout_page/widgets.py
@@ -8,16 +8,17 @@ class LayoutSelector(Select):
     This was adapted from `fluent_pages/pagetypes/fluent_pages/widgets
     .py` in the `django-fluent-pages` app.
     """
-    def render(self, name, value, attrs=None, choices=()):
+    def render(self, name, value, attrs=None, *args, **kwargs):
         """
         Modified render to set the data original value.
 
         :param name: The name of the `Select` field.
         :param value: The value of the `Select` field.
         :param attrs: Additional attributes of the `Select` field.
-        :param choices: Available choices for the `Select` field.
+        :param args: pass along any other arguments.
+        :param kwargs: pass along any other keyword arguments.
         :return: HTML select.
         """
         if attrs:
             attrs['data-original-value'] = value
-        return super(LayoutSelector, self).render(name, value, attrs, choices)
+        return super(LayoutSelector, self).render(name, value, attrs, *args, **kwargs)
