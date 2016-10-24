a49e0eec101d018dec37eafc7d8dd0299505e0b4
Stuart Dines <me@stuartdines.com>
2016-10-24 21:07:04 +1100

    Remove `patterns` use.

--- mezzanine_fluent_pages/tests/urls.py
+++ mezzanine_fluent_pages/tests/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-)
+]
